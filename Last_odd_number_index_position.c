337310f5c209312e8cb215cace8d9aa93d5c9efd
Saimanikantakoppireddy <147487631+Saimanikantakoppireddy@users.noreply.github.com>
2023-12-19 20:57:11 +0530

    #include<stdio.h> int main() {     int n,i,ev=0,odd=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }
        for(i=n-1;i>=0;i--)
        {
            if(a[i]%2!=0)
            {
                printf("%d",i);
                break;
            }
        }
    }

---
+++ Last_odd_number_index_position.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,ev=0,odd=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    
+    for(i=n-1;i>=0;i--)
+    {
+        if(a[i]%2!=0)
+        {
+            printf("%d",i);
+            break;
+        }
+    }
+}
